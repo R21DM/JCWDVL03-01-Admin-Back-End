6e9ed2c68550f1e999871fbf98a062aa2874b0b1
r21dm <rockydwimakmur@gmail.com>
2022-03-18 02:39:04 +0700

    Add get txn by ID

--- routers/transactionRouters.js
+++ routers/transactionRouters.js
@@ -3,5 +3,6 @@ const { transactionController } = require("../controllers");
 const routers = express.Router();
 
 routers.get("/", transactionController.getTransactions);
+routers.get("/:id", transactionController.getTransactionsByID);
 
 module.exports = routers;
